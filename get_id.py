0467133a81facf6c7a650533e174c40ca769bc61
LJ1502 <leejarrell15@gmail.com>
2026-04-24 01:41:22 +0800

    Session Handler

---
+++ get_id.py
@@ -0,0 +1,17 @@
+import asyncio
+asyncio.set_event_loop(asyncio.new_event_loop())
+from pyrogram import Client
+
+api_id = "38782884"       # Paste your api_id here
+api_hash = "d5c53267a5244c82ec90dc3569996e82"   # Paste your api_hash here
+
+# Initialize the client
+app = Client("my_account", api_id=api_id, api_hash=api_hash)
+
+async def main():
+    async with app:
+        # Loop through all your dialogs (chats) and print their IDs
+        async for dialog in app.get_dialogs():
+            print(f"{dialog.chat.title or dialog.chat.first_name}: {dialog.chat.id}")
+
+app.run(main())
